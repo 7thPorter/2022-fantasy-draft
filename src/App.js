48177a07e89b2c143d613bdb9383261cee6891fe
Rusty Porter <rusty.porter@gmail.com>
2022-08-10 20:39:39 -0700

    Deleted manifest.json, added user input state and team list state, began design for field and bottom marquee

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 import "./App.css";
-import Team from "./Team";
+// import Team from "./Team";
 
 function App() {
+  const [userInput, setUserInput] = useState("");
+  const [teamList, setTeamList] = useState([]);
+
+  const handleInput = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTeamList([...teamList, userInput]);
+    setUserInput("");
+  };
+
+  const handleReset = () => {
+    setTeamList([]);
+    setUserInput("");
+  };
+
   return (
     <div id="site">
       <div id="inputs">
         <h3>Enter your league's teams here:</h3>
-        <form></form>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Team Name:
+            <input type="text" value={userInput} onChange={handleInput} />
+          </label>
+          <input type="submit" value="Add Team" />
+        </form>
+        <button onClick={handleReset}>Reset League</button>
       </div>
       <div id="outputs">
         <div id="field">
-          <Team />
-          <Team />
+          <div id="football-field">
+            <div className="endzone">
+              <h1>TOUCHDOWN</h1>
+            </div>
+            <div className="main-field"></div>
+            <div className="endzone">
+              <h1>TOUCHDOWN</h1>
+            </div>
+          </div>
+          <div id="runner">Test Runner</div>
         </div>
-        <div id="scoreboard"></div>
+        <div id="scoreboard">
+          {teamList.length
+            ? teamList.map((team, index) => {
+                return (
+                  <p>This is a placeholder until I think of something better</p>
+                );
+              })
+            : "No teams have been added to the league yet. Add teams above!"}
+        </div>
       </div>
     </div>
   );
