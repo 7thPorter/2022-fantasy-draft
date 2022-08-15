13e208ca907e13ffd05f5eafe6b696a50b9e096b
Rusty Porter <rusty.porter@gmail.com>
2022-08-15 11:13:47 -0700

    Added points progress bars - MOST of the initial groundwork is done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import "./App.css";
 import colorSelect from "./colorGenerator";
 
 function App() {
+  const fieldNumbers = [
+    5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 45, 40, 35, 30, 25, 20, 15, 10, 5,
+  ];
   const [userInput, setUserInput] = useState("");
   const [teamList, setTeamList] = useState([]);
   // const [winnersCircle, setWinnersCircle] = useState([]);
@@ -19,6 +22,7 @@ function App() {
       {
         name: userInput,
         color: `rgb(${colorSelect()}, ${colorSelect()}, ${colorSelect()})`,
+        points: 0,
       },
     ]);
     setUserInput("");
@@ -56,14 +60,33 @@ function App() {
                   })
                 : "No teams have been added to the league yet. Add teams above!"}
             </div>
-            <div className="main-field"></div>
+            <div className="main-field">
+              {fieldNumbers.map((number) => {
+                return <span>{number}</span>;
+              })}
+              <div className="points-display">
+                {teamList.map((team) => {
+                  return (
+                    <div
+                      className="team-points"
+                      style={{
+                        backgroundColor: team.color,
+                        width: `${team.points}%`,
+                      }}
+                    ></div>
+                  );
+                })}
+              </div>
+            </div>
             <div className="endzone">
               <h1>TOUCHDOWN</h1>
             </div>
           </div>
           <div id="runner">Test Runner</div>
         </div>
-        <div id="scoreboard"></div>
+        <div id="scoreboard">
+          <h1>Draft Order:</h1>
+        </div>
       </div>
     </div>
   );
